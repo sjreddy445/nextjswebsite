923a43e8cbacba1dda8e2c78f88a5b4e1962f2ba
prashil117 <prashil117@gmail.com>
2021-07-08 17:53:24 +0530

    ss issues

--- Payloads/Mobility/screenshots.js
+++ Payloads/Mobility/screenshots.js
@@ -1,18 +1,18 @@
 export const payload = [
     {
-        img: require("../../Assets/images/Products/mobility/screenshots/Mobility-1.jpg").default.src,
+        img: require("../../Assets/images/Products/mobility/screenshots/mobility-1.jpg").default.src,
         imgAlt: "AI based workforce allocation"
     },
     {
-        img: require("../../Assets/images/Products/mobility/screenshots/Mobility-2.jpg").default.src,
+        img: require("../../Assets/images/Products/mobility/screenshots/mobility-2.jpg").default.src,
         imgAlt: "AI based workforce allocation"
     },
     {
-        img: require("../../Assets/images/Products/mobility/screenshots/Mobility-3.jpg").default.src,
+        img: require("../../Assets/images/Products/mobility/screenshots/mobility-3.jpg").default.src,
         imgAlt: "AI based workforce allocation"
     },
     {
-        img: require("../../Assets/images/Products/mobility/screenshots/Mobility-4.jpg").default.src,
+        img: require("../../Assets/images/Products/mobility/screenshots/mobility-4.jpg").default.src,
         imgAlt: "AI based workforce allocation"
     },
 
