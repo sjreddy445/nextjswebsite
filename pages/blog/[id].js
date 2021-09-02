185d703f6b2029013e02c01e66a5202ab1d2feee
prashil117 <prashil117@gmail.com>
2021-09-02 14:57:51 +0530

    blog page changes

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -24,7 +24,7 @@ class BlogPost extends Component {
   componentDidMount() {
     setNavColor("transparent-bg");
     window.scrollTo({
-      top: 200,
+      top: 0,
       left: 0,
       behavior: 'smooth'
     })
