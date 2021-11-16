d73a775ab4d18ebde247c403f5a4173a53a803ee
prashil117 <prashil117@gmail.com>
2021-11-16 12:11:30 +0530

    build created

--- pages/careers/Contact.js
+++ pages/careers/Contact.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export default class Contact extends Component {
   render() {
-    console.log("this.props.",this.props)
     return (
       <div className="ml-4 my-4">
         <span className="text-sm"><b>{this.props?.data?.title}  <a style={{ color: '#007bff' }} href={`mailto:${this.props?.data?.description}`}>{this.props?.data?.description}</a></b></span>
