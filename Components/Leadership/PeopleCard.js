63fdefe7c5b6886c2dd64bb41a89ffff0398c80f
prashil117 <prashil117@gmail.com>
2022-01-06 12:28:13 +0530

    linked in issued

--- Components/Leadership/PeopleCard.js
+++ Components/Leadership/PeopleCard.js
@@ -22,7 +22,7 @@ export default class PeopleCard extends Component {
           </div>
           {people.IsShowLinkedIn ?
             <div className='ml-1'>
-              <img onClick={() => window.open(people.linkedIn,"_blank")} src={AddCmsImgBaseUrl('/uploads/linkedin_d0a0d76670.svg')} alt="" height={24} width={24} />
+              <img onClick={() => window.open(people?.linkedIn,"_blank")} src={AddCmsImgBaseUrl('/uploads/linkedin_d0a0d76670.svg')} alt="" height={24} width={24} />
             </div>
             :
             <div className={styles.peopleDesc + " mt-4 text-md pr-2"}>
