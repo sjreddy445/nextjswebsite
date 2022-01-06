f8e2eec0bf7b41bf7db641bf773e41bc52fb05ef
prashil117 <prashil117@gmail.com>
2022-01-06 12:20:55 +0530

    image issure resolved

--- Components/Cards/RatioCard/dynamicRatioCard.js
+++ Components/Cards/RatioCard/dynamicRatioCard.js
@@ -5,20 +5,20 @@ import { withRouter } from 'next/router'
 class RatioCard extends Component {
 
   handleClick = (url) => {
-    this.props.router.push("/products/"+url)
+    this.props.router.push("/products/" + url)
   }
 
   render() {
     let { data } = this.props
     return (
-      <div className={styles.ratiocard + ` pointer ${this.props.ratio}`} style={{ backgroundImage: `url('${AddCmsImgBaseUrl(data.imgUrl.url)}')`, backgroundColor: data.bgColor }} onClick={() => window.open(data.pageUrl, '_blank')}>
+      <div className={styles.ratiocard + ` pointer ${this.props?.ratio}`} style={{ backgroundImage: `url('${AddCmsImgBaseUrl(data?.imgUrl?.url)}')`, backgroundColor: data?.bgColor }} onClick={() => window.open(data.pageUrl, '_blank')}>
         <div className={styles.contentbox}>
-          <div className={styles.logo}>
-            <img src={AddCmsImgBaseUrl(data.logo.url)} alt={data.alt} className="img-fluid" />
-          </div>
-          <div className={data.captionClass}>
+          {data?.logo?.url ? <div className={styles.logo}>
+            <img src={AddCmsImgBaseUrl(data?.logo?.url)} alt={data?.alt} className="img-fluid" />
+          </div> : ""}
+          <div className={data?.captionClass}>
             <span>
-              {data.caption}
+              {data?.caption}
             </span>
           </div>
         </div>
