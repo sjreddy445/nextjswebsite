0b1f51e1689689d198cc0417c3e74b71b092234d
prashil117 <prashil117@gmail.com>
2022-02-19 11:04:20 +0530

    build added

--- next.config.js
+++ next.config.js
@@ -14,12 +14,33 @@ module.exports = {
 module.exports = {
   webpack: (config, { isServer }) => {
     // if (isServer) {
-      require('./scripts/generate-sitemap');
+    require('./scripts/generate-sitemap');
     // }
 
     return config;
   }
 };
+
+// module.exports = {
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       '/': { page: '/' },
+//       '/about': { page: '/about' },
+//       '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+//       '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+//       '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+//     }
+//   },
+// }
+
+
+module.exports = {
+   trailingSlash: true,
+}
+
 module.exports = {
   images: {
     minimumCacheTTL: 60,
@@ -27,3 +48,4 @@ module.exports = {
     path: 'api.getedge.ai',
   }
 }
+
